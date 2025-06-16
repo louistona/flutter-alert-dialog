e43709669037d54a7364dd48e13bfa69e717ed92
toussaint10 <toussaintlm10@gmail.com>
2025-06-17 01:24:35 +0200

    Initial Commit

---
+++ alert_dialog_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [AlertDialog].
+
+void main() => runApp(const AlertDialogExampleApp());
+
+class AlertDialogExampleApp extends StatelessWidget {
+  const AlertDialogExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(colorSchemeSeed: const Color(0xff6750a4)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'AlertDialog',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(child: DialogExample()),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Show Dialog'),
+    );
+  }
+}
